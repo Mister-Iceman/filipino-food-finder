337b5339c472f2b2e53f836e97e0cb0739236b2d
Mister-Iceman <jcrb.hubspace@gmail.com>
2026-02-21 15:16:43 -0800

    fix: add redirects for WordPress lorem ipsum junk and Philadelphia placeholder pages

--- next.config.ts
+++ next.config.ts
@@ -75,7 +75,29 @@ const nextConfig: NextConfig = {
 
       // GROUP 4: Invalid/broken paths
       { source: '/restaurants/category/hotels', destination: '/restaurants', permanent: true },
-      { source: '/$',                           destination: '/',            permanent: true },
+      { source: '/
+
+    ];
+  },
+
+};
+
+export default nextConfig;,                           destination: '/',            permanent: true },
+
+      // GROUP 5: WordPress lorem ipsum junk
+      { source: '/the-standard-chunk-of-lorem-ipsum-used-since-the-1500s-is-reproduced-below-for-those-interested', destination: '/', permanent: true },
+      { source: '/lorem-ipsum-is-simply-dummy-text-of-the-printing-and-typesetting-industry', destination: '/', permanent: true },
+      { source: '/where-does-it-come-from',  destination: '/', permanent: true },
+      { source: '/reset',                    destination: '/', permanent: true },
+      { source: '/category/trends',          destination: '/', permanent: true },
+      { source: '/restaurants/tags/food',    destination: '/', permanent: true },
+      { source: '/comments/feed',            destination: '/', permanent: true },
+      { source: '/feed',                     destination: '/', permanent: true },
+
+      // GROUP 6: Old WordPress Philadelphia restaurant pages
+      { source: '/restaurants/united-states/pennsylvania/philadelphia/loews-philadelphia-hotel', destination: '/restaurants', permanent: true },
+      { source: '/restaurants/united-states/pennsylvania/philadelphia/parc',                     destination: '/restaurants', permanent: true },
+      { source: '/restaurants/united-states/pennsylvania/philadelphia/zavino-pizzeria-and-wine-bar', destination: '/restaurants', permanent: true },
 
     ];
   },
