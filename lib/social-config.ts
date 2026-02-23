24eabe8c9ca94b1316f8e404e40dfccc2250f0b6
Mister-Iceman <jcrb.hubspace@gmail.com>
2026-02-23 13:23:02 -0800

    Add TikTok to social links

--- lib/social-config.ts
+++ lib/social-config.ts
@@ -1,6 +1,5 @@
 // Social Media Configuration
 // Add new platforms here - they'll automatically appear in header and footer
-
 export interface SocialPlatform {
   name: string;
   url: string;
@@ -8,7 +7,6 @@ export interface SocialPlatform {
   ariaLabel: string;
   hoverColor: string;
 }
-
 export const socialPlatforms: SocialPlatform[] = [
   {
     name: 'Instagram',
@@ -17,26 +15,18 @@ export const socialPlatforms: SocialPlatform[] = [
     ariaLabel: 'Follow us on Instagram',
     hoverColor: '#D1880D',
   },
-  // Uncomment when ready to add:
-  // {
-  //   name: 'Facebook',
-  //   url: 'https://www.facebook.com/YOUR_PAGE',
-  //   icon: 'FaFacebook',
-  //   ariaLabel: 'Follow us on Facebook',
-  //   hoverColor: '#62438D',
-  // },
-  // {
-  //   name: 'TikTok',
-  //   url: 'https://www.tiktok.com/@YOUR_HANDLE',
-  //   icon: 'FaTiktok',
-  //   ariaLabel: 'Follow us on TikTok',
-  //   hoverColor: '#BF2F26',
-  // },
-  // {
-  //   name: 'Twitter',
-  //   url: 'https://twitter.com/YOUR_HANDLE',
-  //   icon: 'FaTwitter',
-  //   ariaLabel: 'Follow us on Twitter/X',
-  //   hoverColor: '#92345A',
-  // },
+  {
+    name: 'Facebook',
+    url: 'https://www.facebook.com/filipinofoodnearme',
+    icon: 'FaFacebook',
+    ariaLabel: 'Follow us on Facebook',
+    hoverColor: '#62438D',
+  },
+  {
+    name: 'TikTok',
+    url: 'https://www.tiktok.com/@filipinofoodnearme',
+    icon: 'FaTiktok',
+    ariaLabel: 'Follow us on TikTok',
+    hoverColor: '#BF2F26',
+  },
 ];
